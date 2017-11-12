f90ce81c9b61e901f866fe95cacbfdb7a2e77c9a
guncoin <support@guncoin.info>
2017-11-12 12:00:18 +0000

    Update genbuild.sh / Remove build date

--- src/init.cpp
+++ src/init.cpp
@@ -644,7 +644,7 @@ bool AppInit2(boost::thread_group& threadGroup)
     if (GetBoolArg("-shrinkdebugfile", !fDebug))
         ShrinkDebugFile();
     LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    LogPrintf("Guncoin version %s (%s)\n", FormatFullVersion(), CLIENT_DATE);
+    LogPrintf("Guncoin version %s\n", FormatFullVersion());
     LogPrintf("Using OpenSSL version %s\n", SSLeay_version(SSLEAY_VERSION));
 #ifdef ENABLE_WALLET
     LogPrintf("Using BerkeleyDB version %s\n", DbEnv::version(0, 0, 0));
