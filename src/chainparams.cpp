df48e0bdc1ba480de7988cfae130833f25e13e52
guncoin <support@guncoin.info>
2017-12-22 12:14:18 +0000

    Revert changes to base58 prefixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,11 +70,11 @@ public:
         vSeeds.push_back(CDNSSeedData("guncoin.info", "seed.guncoin.info"));
         vSeeds.push_back(CDNSSeedData("guncoin.info", "seed2.guncoin.info"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(39);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(167);
-        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(39);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(5);
+        base58Prefixes[SECRET_KEY] =     list_of(167);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4);
 
         // Convert the pnSeeds array into usable address objects.
         for (unsigned int i = 0; i < ARRAYLEN(pnSeed); i++)
@@ -136,11 +136,11 @@ public:
         vSeeds.push_back(CDNSSeedData("guncoin.info", "testnet-seed1.guncoin.info"));
         vSeeds.push_back(CDNSSeedData("guncoin.info", "testnet-seed2.guncoin.info"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(111);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(196);
-        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(239);
-        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(111);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(196);
+        base58Prefixes[SECRET_KEY]     = list_of(239);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94);
     }
     virtual Network NetworkID() const { return CChainParams::TESTNET; }
 };
