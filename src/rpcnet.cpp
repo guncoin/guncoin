3eb96e5da5b9fa72567416305c7ad1df0283929e
Peter Bushnell <bushsolo@gmail.com>
2017-12-28 12:20:26 +0000

    RPC strings rename

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -228,7 +228,7 @@ Value getaddednodeinfo(const Array& params, bool fHelp)
             "    \"connected\" : true|false,          (boolean) If connected\n"
             "    \"addresses\" : [\n"
             "       {\n"
-            "         \"address\" : \"192.168.0.201:42954\",  (string) The bitcoin server host and port\n"
+            "         \"address\" : \"192.168.0.201:42954\",  (string) The guncoin server host and port\n"
             "         \"connected\" : \"outbound\"           (string) connection, inbound or outbound\n"
             "       }\n"
             "       ,...\n"
