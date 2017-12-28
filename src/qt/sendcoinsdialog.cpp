33b3b7b0ae83c2713dd2900da7e34de39473277b
Peter Bushnell <bushsolo@gmail.com>
2017-12-28 10:55:28 +0000

    GUI strings

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -738,7 +738,7 @@ void SendCoinsDialog::coinControlChangeEdited(const QString& text)
         }
         else if (!addr.IsValid()) // Invalid address
         {
-            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Bitcoin address"));
+            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Guncoin address"));
         }
         else // Valid address
         {
