9317102ad069c2d680af3e7c77f2c2eebae13c35
Peter Bushnell <bushsolo@gmail.com>
2018-01-24 11:45:01 +0000

    Chainparams

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 8332;
+        nRPCPort = 42953;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 18332;
+        nRPCPort = 52953;
         strDataDir = "testnet3";
     }
 };
@@ -59,7 +59,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 18332;
+        nRPCPort = 52953;
         strDataDir = "regtest";
     }
 };
