5188818f207dc7d2c1467d3906df46161adf1b91
Peter Bushnell <bushsolo@gmail.com>
2018-01-25 09:29:44 +0000

    BTC > GUN

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("GUN");
+    case mBTC: return QString("mGUN");
+    case uBTC: return QString::fromUtf8("μGUN");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Guncoins");
+    case mBTC: return QString("Milli-Guncoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Guncoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
