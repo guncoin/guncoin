06eaa08e6c5ed34e5da30e6113a3dcfa4b5252c7
Peter Bushnell <bushsolo@gmail.com>
2018-06-04 09:51:03 +0100

    Adjust fee system

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -44,9 +44,9 @@ static const unsigned int DEFAULT_KEYPOOL_SIZE = 1000;
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
-static const CAmount DEFAULT_FALLBACK_FEE = 20000;
+static const CAmount DEFAULT_FALLBACK_FEE = 200000;
 //! -m_discard_rate default
-static const CAmount DEFAULT_DISCARD_FEE = 10000;
+static const CAmount DEFAULT_DISCARD_FEE = 100000;
 //! -mintxfee default
 static const CAmount DEFAULT_TRANSACTION_MINFEE = 1000;
 //! minimum recommended increment for BIP 125 replacement txs
